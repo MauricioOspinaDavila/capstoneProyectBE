438ede184df765b66cdfda1504db838a4e1d7382
mauricioospina <jairospina@outlook.com>
2024-05-19 10:29:19 -0500

    archivo para configuracion de BBDD

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    username: "your_db_user",
+    password: "your_db_password",
+    database: "your_db_name",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+};
